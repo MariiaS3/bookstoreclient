0aa69799a580fba3da0d6e4938bf3021c5922648
MariiaS3 <mariia.sydor28082001@gmail.com>
2022-12-20 23:03:54 +0100

    add axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
+import Axios from "axios";
+import baseUrl from "./config/index";
+
 
 const App = () =>{
+
+    Axios(`${baseUrl}/api/v1/books`).then(books =>{
+        console.log(books);
+    });
+
     return <div>My first component</div>
   }
 
