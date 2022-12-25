74823db712f2dadb73a8f9e0cf091c06c5d85507
MariiaS3 <mariia.sydor28082001@gmail.com>
2022-12-26 00:11:53 +0100

    check

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const enviromentUrl = new Map();
 
 enviromentUrl.set('localhost', 'http://localhost:8080');
-enviromentUrl.set('book-store1-web', 'https://book-demo1-store.herokuapp.com');
+enviromentUrl.set('book-store1-web.herokuapp.com/', 'https://book-demo1-store.herokuapp.com');
 
 export default enviromentUrl.get(window.location.hostname);
