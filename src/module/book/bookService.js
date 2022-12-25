c4131739e5fab223a392a13f7eaaa5e2b200e5d8
MariiaS3 <mariia.sydor28082001@gmail.com>
2022-12-26 00:01:54 +0100

    check

--- src/module/book/bookService.js
+++ src/module/book/bookService.js
@@ -1,6 +1,8 @@
 import Axios from "axios";
 import baseUrl from '../../config'
 
-const getBookService = () => Axios.get(`${baseUrl}/api/v1/books`)
+// const getBookService = () => Axios.get(`${baseUrl}/api/v1/books`)
+const getBookService = () => Axios.get(`https://book-demo1-store.herokuapp.com/api/v1/books`)
+
 
 export default getBookService;
