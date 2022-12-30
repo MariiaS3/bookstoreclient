6c2b2f115bb8911ca6d8df5f071f6f526efdaf66
MariiaS3 <mariia.sydor28082001@gmail.com>
2022-12-30 23:27:42 +0100

    fix  tests

--- src/module/user/__tests__/userAction.test.js
+++ src/module/user/__tests__/userAction.test.js
@@ -52,8 +52,8 @@ describe("login action", () =>{
 
         const actions =store.getActions();
 
-        expect(actions.length).toEqual(1);
-        expect(actions[0]).toEqual({
+        expect(actions.length).toEqual(3);
+        expect(actions[1]).toEqual({
             type: 'USER_REGISTER',
             payload:{
                 id: "some uuid",
